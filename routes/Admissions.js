dc8060302341d44c0525113b834c91336ba71d82
Shoinbek <shoinbekov23@gmail.com>
2024-03-17 19:25:20 -0500

    Sign Up sheet

--- routes/Admissions.js
+++ routes/Admissions.js
@@ -2,11 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/Admissions', function(req, res, next) {
+router.get('/', function(req, res, next) {
   res.render('Admissions');
 });
 
-router.get('/Admissions/Tour', function(req, res, next) {
+router.get('/Tour', function(req, res, next) {
   res.render('TourSignUpSheet');
 });
 
