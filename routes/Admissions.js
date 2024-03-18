b87f8a66f6ed3bf32627183375ddef492fa4c616
Shoinbek <shoinbekov23@gmail.com>
2024-03-18 09:00:18 -0500

    some update

--- routes/Admissions.js
+++ routes/Admissions.js
@@ -10,4 +10,22 @@ router.get('/Tour', function(req, res, next) {
   res.render('TourSignUpSheet');
 });
 
+// Server side validation
+router.post('/TourSignUp', function(req, res, next){
+  if(req.body.firstname === '' || req.body.lastname === '' || req.body.email === '' || req.body.phone === '' ||
+  req.body.tourDataTime === '' || req.body.areaOfInterest === '') {
+    res.send('Please fill out all fields before signing up.');
+  }
+
+  console.log(req.body.firstname);
+  console.log(req.body.lastname);
+  res.render('TourSignUpMembers', {
+    firstname: req.body.firstname,
+    lastname: req.body.lastname
+
+  });
+});
+
+
+
 module.exports = router;
